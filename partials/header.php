5cded3de13ac8bbcacbef13b0f1a6162700b0986
simonetocco8-web <simonetocco8@gmail.com>
2026-09-18 19:38:52 +0200

    Add parking availability dashboard card

--- partials/header.php
+++ partials/header.php
@@ -37,6 +37,7 @@ $magazzinoMenuOpen = str_starts_with($currentPath, 'product_') || nav_path_is_cu
 ], $currentPath);
 $personaleMenuOpen = nav_path_is_current(['days_off_list.php', 'days_off_create.php', 'overtime.php', 'overtime_monthly.php', 'absence.php', 'send_sms.php'], $currentPath);
 $riassettiMenuOpen = nav_path_is_current(['riassetti.php', 'riassetti_statistiche.php'], $currentPath);
+$parkingMenuOpen = nav_path_is_current(['parking.php', 'parking_settings.php'], $currentPath);
 $avanzateMenuOpen = nav_path_is_current(['settings.php', 'users.php', 'user_create.php', 'user_edit.php', 'daily_summary_pdf.php'], $currentPath);
 $canSeePersonaleMenu = $user && (user_is_amministrazione($user) || user_can_send_sms($user));
 $canSeeAvanzateMenu = $user && (user_is_admin($user) || user_is_amministrazione($user));
@@ -247,6 +248,10 @@ $canSeeAvanzateMenu = $user && (user_is_admin($user) || user_is_amministrazione(
         <?php endif; ?>
         <?php if($user && user_is_reception_or_amministrazione($user)): ?>
         <li class="nav-item"><a class="nav-link" href="<?= e($base) ?>/tasks.php"><i class="bi bi-check2-square"></i><span>Task</span></a></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle <?= $parkingMenuOpen ? 'active' : '' ?>" href="#" id="parkingSidebarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="<?= $parkingMenuOpen ? 'true' : 'false' ?>"><i class="bi bi-p-square"></i><span>Parcheggi</span></a>
+          <ul class="dropdown-menu <?= $parkingMenuOpen ? 'show' : '' ?>" aria-labelledby="parkingSidebarDropdown"><li><a class="dropdown-item" href="<?= e($base) ?>/parking.php"><i class="bi bi-map"></i><span>Mappa</span></a></li><li><a class="dropdown-item" href="<?= e($base) ?>/parking_settings.php"><i class="bi bi-sliders"></i><span>Setting</span></a></li></ul>
+        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle <?= $trasportiMenuOpen ? 'active' : '' ?>" href="#" id="trasportiSidebarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="<?= $trasportiMenuOpen ? 'true' : 'false' ?>"><i class="bi bi-car-front"></i><span>Trasporti</span></a>
           <ul class="dropdown-menu <?= $trasportiMenuOpen ? 'show' : '' ?>" aria-labelledby="trasportiSidebarDropdown">
@@ -523,6 +528,10 @@ $canSeeAvanzateMenu = $user && (user_is_admin($user) || user_is_amministrazione(
             <?php endif; ?>
             <?php if($user && user_is_reception_or_amministrazione($user)): ?>
             <li class="nav-item"><a class="nav-link" href="<?= e($base) ?>/tasks.php"><i class="bi bi-check2-square"></i><span>Task</span></a></li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle <?= $parkingMenuOpen ? 'active' : '' ?>" href="#" id="parkingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="<?= $parkingMenuOpen ? 'true' : 'false' ?>"><i class="bi bi-p-square"></i><span>Parcheggi</span></a>
+              <ul class="dropdown-menu <?= $parkingMenuOpen ? 'show' : '' ?>" aria-labelledby="parkingDropdown"><li><a class="dropdown-item" href="<?= e($base) ?>/parking.php"><i class="bi bi-map"></i><span>Mappa</span></a></li><li><a class="dropdown-item" href="<?= e($base) ?>/parking_settings.php"><i class="bi bi-sliders"></i><span>Setting</span></a></li></ul>
+            </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle <?= $trasportiMenuOpen ? 'active' : '' ?>" href="#" id="trasportiDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="<?= $trasportiMenuOpen ? 'true' : 'false' ?>"><i class="bi bi-car-front"></i><span>Trasporti</span></a>
               <ul class="dropdown-menu <?= $trasportiMenuOpen ? 'show' : '' ?>" aria-labelledby="trasportiDropdown">
